49386346d07c8e7823e6bff771d055d83aa29c3c
Stuart <stuartrure@gmail.com>
2023-07-18 16:11:35 +0100

    mvp finished, extensions started

--- controllers/orders_controller.py
+++ controllers/orders_controller.py
@@ -3,6 +3,15 @@ from models.orders_list import orders
 
 order_blueprint= Blueprint("orders", __name__) 
 
-@order_blueprint.route("/")
+@order_blueprint.route("/orders/")
 def index():
     return render_template("index.html", title= "My orders", orders_list= orders)
+
+
+@order_blueprint.route("/orders/<int:index>")
+def display_order(index):
+    if index < len(orders):
+        order = orders[index]
+        return render_template("order.html", order=order)
+    else:
+        return "Order not found"
--- models/orders_list.py
+++ models/orders_list.py
@@ -2,4 +2,6 @@ from models.order import Order
 
 order1 = Order("Callum", "18th July", "IPA", 5, "A juicy pint")
 order2 = Order("Tony", "17th July", "WeisseBeir", 2, "A dry German style")
-orders = [order1, order2]
+order3 = Order("Shaun", "15th July", "Lager", 20,  "Easy drinking")
+order4 = Order("Benny", "18th July", "Stout", 4, "chocolatey notes" )
+orders = [order1, order2, order3, order4]
